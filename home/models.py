e346f7db6f3906a7d180a8dfa8a617acc580b2cf
Gourav Kumar <Gourav200056@gmail.com>
2022-04-01 13:58:37 +0530

    Small change

--- home/models.py
+++ home/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Job(models.Model):
     company = models.CharField(max_length=100)
     title = models.CharField(max_length=200)
-    description = models.TextField(max_length=1000)
+    description = models.TextField(max_length=10000)
     location = models.CharField(max_length=100)
-    url = models.URLField(max_length=200)
+    url = models.URLField(max_length=1000)
     logo = models.ImageField(null=True, blank=True, upload_to='company_logos')
 
     def __str__ (self):
